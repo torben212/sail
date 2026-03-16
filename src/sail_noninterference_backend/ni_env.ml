be4b7eae4f7461ba25584b21b32462aef0f84cdd
Marcus Rørby Therkildsen <marcustherkild@gmail.com>
2026-03-16 15:43:21 +0100

    jeg har implementeret et env der burde holde øje med public/secret variabler. Nu er det bare at implementere logikken

---
+++ src/sail_noninterference_backend/ni_env.ml
@@ -0,0 +1,22 @@
+open Libsail
+open Ast
+open Ast_util
+open Ast_compare
+
+type lattice = Public | Secret
+
+module IdMap = Map.Make(struct type t = id let compare = compare end)
+
+type ni_env = {
+  lattices : lattice IdMap.t;
+}
+
+let empty_ni_env = {
+  lattices = IdMap.empty;
+}
+
+let add (id: id) (lat: lattice) (env: ni_env) : ni_env =
+  { lattices = IdMap.add id lat env.lattices }
+
+let find (id: id) (env: ni_env) : lattice =
+  IdMap.find id env.lattices
--- src/sail_noninterference_backend/sail_plugin_noninterference.ml
+++ src/sail_noninterference_backend/sail_plugin_noninterference.ml
@@ -7,13 +7,13 @@ open Jib
 open Jib_util
 open Value2
 open Printf
+open Ni_env
 
 module Callgraph_commands = Callgraph_commands
 
 (* Options for noninterference plugin *)
 let opt_output_dir = ref (Some ".")
 let opt_verbose = ref false
-
 let noninterference_options =
   [
     ( Flag.create ~prefix:["noninterference"] ~arg:"directory" "output_dir",
@@ -28,14 +28,12 @@ let noninterference_options =
 
 
 
-let check_variable_lattice (env: Type_check.env) (id: id) : unit =
+let check_variable_lattice (ni_env : ni_env) (id: id) : ni_env = (*The purpose of this function is to update our ni_env with lattice information of each var*)
   let var_name = string_of_id id in
   if String.starts_with ~prefix:"public_" var_name then
-    (* variable is public *)
-    ()
+    add id Public ni_env
   else if String.starts_with ~prefix:"secret_" var_name then
-    (* variable is secret *)
-    ()
+    add id Secret ni_env
   else
     failwith ("Variable " ^ var_name ^ " does not follow naming convention for public or secret variables")
 
@@ -45,23 +43,19 @@ let is_binop id =
   | "+" | "-" | "*" | "/" | "&&" | "||" | "==" | "!=" | "<" | ">" | "<=" | ">=" -> true
   | _ -> false
 
-let rec check_expr (env : Type_check.env) (expr : 'a exp) : unit = 
+let rec check_expr (env : Type_check.env) (expr : 'a exp) (ni_env : ni_env) : unit = 
   match expr with
   | E_aux (E_app (id, [e1; e2]), _) when is_binop id ->
-      check_expr env e1;
-      check_expr env e2;
-      (*let op = string_of_id id in
-      match op with 
+      check_expr env e1 ni_env;
+      check_expr env e2 ni_env;
+      
       (*
       Any operator is treated the same in non-interference analysis since it does not matter
       what the operator is. If we have an expression like "x + y", the operator doesn't affect
       the non-interference properties since what we are really interested in, is whether x or y
       are secret and whether they are assigned to a public or secret variable.
       *)
-      | "+" | "-" | "*" | "/" -> () 
-      | "&&" | "||" -> ()
-      | "==" | "!=" | "<" | ">" | "<=" | ">=" -> ()
-      | _ -> failwith ("Unsupported binary operator: " ^ op)*)
+      
   | E_aux (E_lit lit, _) ->
     (match lit with
     | L_aux (L_num _, _) -> ()
@@ -83,23 +77,24 @@ let rec check_expr (env : Type_check.env) (expr : 'a exp) : unit =
     - If lexp is secret and value is public, then the assignment is fine.
     - If lexp is secret and value is secret, then the assignment is fine.
     - If lexp is public and value is secret, then this is a violation of non-interference and should result in a skip.
-    For optimization we should check lexp first and find out whether it is public. If it is secret, then
-    we can skip checking value since the assignment is fine.
+    For optimization we should check value first and find out whether it is public. If it is secret, then
+    we can skip checking lexp since the assignment is fine.
     *)
-      check_lexp env lexp;
-      check_expr env value;
+      
+      check_lexp env lexp ni_env;
+      check_expr env value ni_env;
       (* handle assignment *)
   | E_aux (E_id id, _) ->
       (*  *)
       ()
   | E_aux (E_if (cond, then_exp, else_exp), _) ->
-      check_expr env cond;
-      check_expr env then_exp;
-      check_expr env else_exp;
+      check_expr env cond ni_env;
+      check_expr env then_exp ni_env;
+      check_expr env else_exp ni_env;
       (* handle if statement *)
   | _ -> ()
 
-and check_lexp (env : Type_check.env) (lexp : 'a lexp) : unit =
+and check_lexp (env : Type_check.env) (lexp : 'a lexp) (ni_env : ni_env) : unit =
   match lexp with
   | LE_aux (LE_id id, _) -> 
     (*
@@ -109,20 +104,24 @@ and check_lexp (env : Type_check.env) (lexp : 'a lexp) : unit =
     every time we encounter a new variable. This can happen in an assignment, a function parameter, or
     a let binding.
     *)
-    (*check_variable_lattice env id;*)
-    ()
-  | LE_aux (LE_deref exp, _) -> check_expr env exp
-  | LE_aux (LE_field (lexp, _), _) -> check_lexp env lexp
+    ni_env' = check_variable_lattice ni_env id;
+    (*If the lexp is public we need to check the value*)
+    match find id ni_env' with
+    | Public -> ()
+    | Secret -> ()
+
+  | LE_aux (LE_deref exp, _) -> check_expr env exp ni_env
+  | LE_aux (LE_field (lexp, _), _) -> check_lexp env lexp ni_env
   | _ -> ()
 
-let check_ast (env : Type_check.env) (ast : Type_check.typed_ast) = 
+let check_ast (env : Type_check.env) (ast : Type_check.typed_ast) (ni_env : ni_env) = 
   List.iter (fun def -> 
     match def with
     | DEF_aux (DEF_fundef (FD_aux (FD_function (_, _, funcls), _)), _) ->
         List.iter 
           (fun (FCL_aux (FCL_funcl (_, pexp), _)) ->
             match pexp with
-            |Pat_aux (Pat_exp (_, body), _) -> check_expr env body
+            |Pat_aux (Pat_exp (_, body), _) -> check_expr env body ni_env
             | _ -> ())
             funcls
     | _ -> ()
@@ -151,7 +150,7 @@ let noninterference_target out_file { ast; effect_info; env; _ } =
     flush_all ()
   );
   
-  check_ast env ast
+  check_ast env ast empty_ni_env
 
 let _ =
   Target.register
